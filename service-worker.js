2a9edd00e9914e30ffc481ae2067875529000d5a
Saichi-Fujimi-Sugimoto <can.be.overcome44@gmail.com>
2025-06-28 23:03:15 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
-// インストール時にキャッシュ
+// インストール時にキャッシュを作成
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -16,7 +16,20 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// リクエスト時にキャッシュ優先で応答
+// アクティベート時に古いキャッシュを削除
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// フェッチ時にキャッシュ優先で応答し、なければネットワークへ
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
